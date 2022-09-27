8f1add27955c1f25319e42455f20533e848a1462
ikvns4 <ikvns4@gmail.com>
2022-09-27 17:23:19 +0530

    first commit

---
+++ Eshop/templatetags/cart_filter.py
@@ -0,0 +1,40 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter(name='isexistincart')
+def isexistincart(product, cart):
+    key = cart.keys()
+    for id in key:
+        if int(id) == product.id:
+            return True
+    return False
+
+
+@register.filter(name='cartquantity')
+def cartquantity(product, cart):
+    key = cart.keys()
+    for id in key:
+        if int(id) == product.id:
+            return cart.get(id)
+    return False
+
+
+@register.filter(name='total_price')
+def total_price(product, cart):
+    tp = product.price * cartquantity(product, cart)
+    return tp
+
+
+@register.filter(name="payable_amount")
+def payable_amount(product, cart):
+    s = 0
+    for i in product:
+        s = s + total_price(i, cart)
+    return s
+
+
+@register.filter(name="total_amount")
+def total_amount(price, quantity):
+    return price*quantity
